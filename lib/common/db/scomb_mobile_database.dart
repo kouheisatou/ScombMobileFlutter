8d78f406fcea8daec6db08dfc9b1a3462395f8eb
kouheisatou <koheisato0420@gmail.com>
2022-09-03 20:24:19 +0900

    ADD : database migration

--- lib/common/db/scomb_mobile_database.dart
+++ lib/common/db/scomb_mobile_database.dart
@@ -12,16 +12,25 @@ import 'class_cell.dart';
 
 part 'scomb_mobile_database.g.dart';
 
-@Database(version: 1, entities: [Setting, ClassCell, Task])
+@Database(version: 2, entities: [Setting, ClassCell, Task])
 abstract class AppDatabase extends FloorDatabase {
   static AppDatabase? _appDatabase;
 
   SettingDao get currentSettingDao;
+
   ClassCellDao get currentClassCellDao;
+
   TaskDao get currentTaskDao;
 
   static Future<AppDatabase> getDatabase() async {
-    return _appDatabase ??=
-        await $FloorAppDatabase.databaseBuilder('scomb_mobile.db').build();
+    return _appDatabase ??= await $FloorAppDatabase
+        .databaseBuilder('scomb_mobile.db')
+        .addMigrations([migration1to2]).build();
   }
 }
+
+final migration1to2 = Migration(1, 2, (database) {
+  print("migration_1_to_2");
+  return database
+      .execute("ALTER TABLE class_cell ADD COLUMN syllabusUrl TEXT;");
+});
