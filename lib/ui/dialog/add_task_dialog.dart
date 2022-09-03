f05ab49f0f7308036fd452a8f24499969ce48462
kouheisatou <koheisato0420@gmail.com>
2022-09-04 00:20:17 +0900

    ADD : loading dialog

--- lib/ui/dialog/add_task_dialog.dart
+++ lib/ui/dialog/add_task_dialog.dart
@@ -37,190 +37,182 @@ class _AddTaskDialogState extends State<AddTaskDialog> {
 
   @override
   Widget build(BuildContext context) {
-    return SimpleDialog(
+    return AlertDialog(
       title: const Text("課題追加"),
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextField(
-                autofocus: true,
-                decoration: const InputDecoration(labelText: "課題タイトル"),
-                onChanged: (text) {
-                  title = text;
-                },
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text("授業 : "),
-                    SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Row(
-                        children: [
-                          DropdownButton<ClassCell?>(
-                            value: classDropDownValue,
-                            items: buildDropdownItems(),
-                            onChanged: (newClassCell) async {
-                              setState(() {
-                                classDropDownValue = newClassCell;
-                              });
-                            },
-                          ),
-                          IconButton(
-                            onPressed: () {
-                              setState(() {
-                                classDropDownValue = null;
-                              });
-                            },
-                            icon: const Icon(Icons.close_sharp),
-                          )
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text("課題タイプ : "),
-                    SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Row(
-                        children: [
-                          DropdownButton<int>(
-                            value: taskTypeDropDownValue,
-                            items: <int>[
-                              TaskType.TASK,
-                              TaskType.TEST,
-                              TaskType.SURVEY,
-                              TaskType.OTHERS
-                            ].map<DropdownMenuItem<int>>((int t) {
-                              return DropdownMenuItem<int>(
-                                value: t,
-                                child: Text(TASK_TYPE_MAP[t]!),
-                              );
-                            }).toList(),
-                            onChanged: (i) async {
-                              setState(() {
-                                taskTypeDropDownValue = i ?? TaskType.OTHERS;
-                              });
-                            },
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text("締め切り日時 : "),
-                    SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Row(
-                        children: [
-                          Text(
-                            selectedDate != null
-                                ? timeToString(
-                                    selectedDate!.millisecondsSinceEpoch)
-                                : "選択されていません",
-                          ),
-                          IconButton(
-                            icon: const Icon(Icons.access_time),
-                            onPressed: () async {
-                              DatePicker.showDateTimePicker(
-                                context,
-                                showTitleActions: true,
-                                theme: const DatePickerTheme(
-                                  backgroundColor: Colors.blue,
-                                  itemStyle: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                  doneStyle: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 16,
-                                  ),
-                                ),
-                                onChanged: (date) {
-                                  if (date.millisecondsSinceEpoch <
-                                      DateTime.now().millisecondsSinceEpoch) {
-                                    Fluttertoast.showToast(
-                                        msg: "現在よりも前の日付は指定できません");
-                                  } else {
-                                    setState(() {
-                                      selectedDate = date;
-                                    });
-                                  }
-                                },
-                                currentTime: selectedDate ??
-                                    (widget.initDate ?? DateTime.now()),
-                                locale: LocaleType.jp,
-                              );
-                            },
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ],
-          ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context, null);
+          },
+          child: const Text("キャンセル"),
         ),
-        Padding(
-          padding: const EdgeInsets.only(right: 10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              TextButton(
-                onPressed: () {
-                  Navigator.pop(context, null);
-                },
-                child: const Text("キャンセル"),
-              ),
-              TextButton(
-                onPressed: () async {
-                  if (title == "") {
-                    Fluttertoast.showToast(msg: "課題タイトルを入力してください");
-                    return;
-                  }
-                  if (selectedDate == null) {
-                    Fluttertoast.showToast(msg: "締め切り日時を入力してください");
-                    return;
-                  }
-                  if (selectedDate!.millisecondsSinceEpoch <
-                      DateTime.now().millisecondsSinceEpoch) {
-                    Fluttertoast.showToast(msg: "今よりの先の時刻を選択してください");
-                    return;
-                  }
+        TextButton(
+          onPressed: () async {
+            if (title == "") {
+              Fluttertoast.showToast(msg: "課題タイトルを入力してください");
+              return;
+            }
+            if (selectedDate == null) {
+              Fluttertoast.showToast(msg: "締め切り日時を入力してください");
+              return;
+            }
+            if (selectedDate!.millisecondsSinceEpoch <
+                DateTime.now().millisecondsSinceEpoch) {
+              Fluttertoast.showToast(msg: "今よりの先の時刻を選択してください");
+              return;
+            }
 
-                  var newTask = Task.userTask(
-                    title,
-                    classDropDownValue,
-                    taskTypeDropDownValue,
-                    selectedDate!.millisecondsSinceEpoch,
-                  );
+            var newTask = Task.userTask(
+              title,
+              classDropDownValue,
+              taskTypeDropDownValue,
+              selectedDate!.millisecondsSinceEpoch,
+            );
 
-                  Navigator.pop(context, newTask);
-                },
-                child: const Text("追加"),
-              ),
-            ],
-          ),
+            Navigator.pop(context, newTask);
+          },
+          child: const Text("追加"),
         ),
       ],
+      content: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextField(
+              autofocus: true,
+              decoration: const InputDecoration(labelText: "課題タイトル"),
+              onChanged: (text) {
+                title = text;
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text("授業 : "),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        DropdownButton<ClassCell?>(
+                          value: classDropDownValue,
+                          items: buildDropdownItems(),
+                          onChanged: (newClassCell) async {
+                            setState(() {
+                              classDropDownValue = newClassCell;
+                            });
+                          },
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            setState(() {
+                              classDropDownValue = null;
+                            });
+                          },
+                          icon: const Icon(Icons.close_sharp),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text("課題タイプ : "),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        DropdownButton<int>(
+                          value: taskTypeDropDownValue,
+                          items: <int>[
+                            TaskType.TASK,
+                            TaskType.TEST,
+                            TaskType.SURVEY,
+                            TaskType.OTHERS
+                          ].map<DropdownMenuItem<int>>((int t) {
+                            return DropdownMenuItem<int>(
+                              value: t,
+                              child: Text(TASK_TYPE_MAP[t]!),
+                            );
+                          }).toList(),
+                          onChanged: (i) async {
+                            setState(() {
+                              taskTypeDropDownValue = i ?? TaskType.OTHERS;
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text("締め切り日時 : "),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        Text(
+                          selectedDate != null
+                              ? timeToString(
+                                  selectedDate!.millisecondsSinceEpoch)
+                              : "選択されていません",
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.access_time),
+                          onPressed: () async {
+                            DatePicker.showDateTimePicker(
+                              context,
+                              showTitleActions: true,
+                              theme: const DatePickerTheme(
+                                backgroundColor: Colors.blue,
+                                itemStyle: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                doneStyle: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              onChanged: (date) {
+                                if (date.millisecondsSinceEpoch <
+                                    DateTime.now().millisecondsSinceEpoch) {
+                                  Fluttertoast.showToast(
+                                      msg: "現在よりも前の日付は指定できません");
+                                } else {
+                                  setState(() {
+                                    selectedDate = date;
+                                  });
+                                }
+                              },
+                              currentTime: selectedDate ??
+                                  (widget.initDate ?? DateTime.now()),
+                              locale: LocaleType.jp,
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
