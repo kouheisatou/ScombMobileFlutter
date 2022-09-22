c6b62c1739c32d2ad82fce789b4c438ddaace040
kouheisatou <koheisato0420@gmail.com>
2022-09-22 19:02:32 +0900

    FIX : on database migration skipped

--- lib/common/db/scomb_mobile_database.dart
+++ lib/common/db/scomb_mobile_database.dart
@@ -28,14 +28,8 @@ abstract class AppDatabase extends FloorDatabase {
         .databaseBuilder('scomb_mobile.db')
         .addMigrations([
       migration1to2,
-      migration1to3,
       migration2to3,
-      migration1to4,
-      migration2to4,
       migration3to4,
-      migration1to5,
-      migration2to5,
-      migration3to5,
       migration4to5,
     ]).build();
   }
@@ -45,60 +39,23 @@ final migration1to2 = Migration(1, 2, (database) {
   return database.execute("ALTER TABLE class_cell ADD COLUMN syllabusUrl TEXT");
 });
 
-final migration1to3 = Migration(1, 3, (database) async {
-  await migrationTo3(database);
-});
-final migration2to3 = Migration(2, 3, (database) async {
-  await migrationTo3(database);
-});
-
-final migration1to4 = Migration(1, 4, (database) async {
-  await migrationTo3(database);
-  await migrationTo4(database);
-});
-final migration2to4 = Migration(2, 4, (database) async {
-  await migrationTo3(database);
-  await migrationTo4(database);
-});
-final migration3to4 = Migration(3, 4, (database) async {
-  await migrationTo4(database);
-});
-
-final migration1to5 = Migration(1, 5, (database) async {
-  await migrationTo3(database);
-  await migrationTo4(database);
-  await migrationTo5(database);
-});
-final migration2to5 = Migration(2, 5, (database) async {
-  await migrationTo3(database);
-  await migrationTo4(database);
-  await migrationTo5(database);
-});
-final migration3to5 = Migration(3, 5, (database) async {
-  await migrationTo4(database);
-  await migrationTo5(database);
-});
-final migration4to5 = Migration(4, 5, (database) async {
-  await migrationTo5(database);
-});
-
 // class_cell primary key change
-Future<void> migrationTo3(sqflite.Database database) async {
+final migration2to3 = Migration(2, 3, (database) async {
   await database.rawQuery("DROP TABLE class_cell;");
   await database.rawQuery(
       "CREATE TABLE class_cell(classId TEXT NOT NULL, period INTEGER NOT NULL, dayOfWeek INTEGER NOT NULL, isUserClassCell INTEGER NOT NULL, timetableTitle TEXT NOT NULL, year INTEGER, term TEXT, name TEXT, teachers TEXT, room TEXT, customColorInt INTEGER, url TEXT, note TEXT, syllabusUrl TEXT, PRIMARY KEY (classId, period, dayOfWeek, isUserClassCell, timetableTitle));");
   await database.rawQuery(
       "INSERT OR REPLACE INTO settings (settingKey, settingValue) values ('${SettingKeys.TIMETABLE_LAST_UPDATE}', '0');");
-}
+});
 
 // class_cell primary key change
-Future<void> migrationTo4(sqflite.Database database) async {
+final migration3to4 = Migration(3, 4, (database) async {
   await database
       .rawQuery("ALTER TABLE class_cell ADD COLUMN numberOfCredit INT;");
-}
+});
 
 // add my_links table
-Future<void> migrationTo5(sqflite.Database database) async {
+final migration4to5 = Migration(4, 5, (database) async {
   await database.rawQuery(
       "CREATE TABLE `my_links` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `title` TEXT NOT NULL, `url` TEXT NOT NULL);");
-}
+});
