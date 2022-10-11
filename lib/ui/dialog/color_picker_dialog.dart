88e7a86a4d3faac4e7f8470cc665923036a46e74
kouheisatou <koheisato0420@gmail.com>
2022-10-11 12:15:07 +0900

    FIX : color pallet overflow

--- lib/ui/dialog/color_picker_dialog.dart
+++ lib/ui/dialog/color_picker_dialog.dart
@@ -11,51 +11,50 @@ class ColorPickerDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: BlockPicker(
-        pickerColor: Colors.black,
-        availableColors: [
-          hexToColor("#FFEF9A9A"),
-          hexToColor("#FFF48FB1"),
-          hexToColor("#FFCE93D8"),
-          hexToColor("#FFB39DDB"),
-          hexToColor("#FF9FA8DA"),
-          hexToColor("#FF90CAF9"),
-          hexToColor("#FF81D4FA"),
-          hexToColor("#FF80DEEA"),
-          hexToColor("#FF80CBC4"),
-          hexToColor("#FFA5D6A7"),
-          hexToColor("#FFC5E1A5"),
-          hexToColor("#FFE6EE9C"),
-          hexToColor("#FFFFF59D"),
-          hexToColor("#FFFFE082"),
-          hexToColor("#FFFFCC80"),
-          hexToColor("#FFFFAB91"),
-          hexToColor("#FFBCAAA4"),
-          // default color
-          defaultColor != null ? defaultColor! : Colors.white70
-        ],
-        layoutBuilder: (context, colors, child) {
-          Orientation orientation = MediaQuery.of(context).orientation;
-          return SizedBox(
+    return BlockPicker(
+      pickerColor: Colors.black,
+      availableColors: [
+        hexToColor("#FFEF9A9A"),
+        hexToColor("#FFF48FB1"),
+        hexToColor("#FFCE93D8"),
+        hexToColor("#FFB39DDB"),
+        hexToColor("#FF9FA8DA"),
+        hexToColor("#FF90CAF9"),
+        hexToColor("#FF81D4FA"),
+        hexToColor("#FF80DEEA"),
+        hexToColor("#FF80CBC4"),
+        hexToColor("#FFA5D6A7"),
+        hexToColor("#FFC5E1A5"),
+        hexToColor("#FFE6EE9C"),
+        hexToColor("#FFFFF59D"),
+        hexToColor("#FFFFE082"),
+        hexToColor("#FFFFCC80"),
+        hexToColor("#FFFFAB91"),
+        hexToColor("#FFBCAAA4"),
+        // default color
+        defaultColor != null ? defaultColor! : Colors.white70
+      ],
+      layoutBuilder: (context, colors, child) {
+        return Center(
+          child: SizedBox(
             width: 300,
-            height: orientation == Orientation.portrait ? 180 : 100,
+            height: 150,
             child: GridView.count(
               physics: const NeverScrollableScrollPhysics(),
-              crossAxisCount: orientation == Orientation.portrait ? 6 : 7,
+              crossAxisCount: 6,
               crossAxisSpacing: 5,
               mainAxisSpacing: 5,
               children: [
                 for (Color color in colors) child(color),
               ],
             ),
-          );
-        },
-        onColorChanged: (color) {
-          selectedColor = color.value;
-          Navigator.pop(context, color.value);
-        },
-      ),
+          ),
+        );
+      },
+      onColorChanged: (color) {
+        selectedColor = color.value;
+        Navigator.pop(context, color.value);
+      },
     );
   }
 }
